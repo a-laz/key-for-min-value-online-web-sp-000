fd49c6820d91477337c48e2544f727d8b08d924e
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-12 00:58:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   smallest_value = 1
-  name_hash.reject{|key, value| value > smallest_value}.collect{|key, value| :key}
+  name_hash.reject{|key, value| value > smallest_value}.collect do |key, value|
+    key.to_a(symbol)
+  end
 end
