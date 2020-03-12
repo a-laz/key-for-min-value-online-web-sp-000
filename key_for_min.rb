3e9fc3de458aa7289fab817884f4dcd3be998ceb
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-12 00:35:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
+  smallest_value = 1
   name_hash.each do |key, value|
     binding.pry
+    if value <= smallest_value
+      smallest_value = value
+    end
   end
+  name_hash[smallest_value]
 end
