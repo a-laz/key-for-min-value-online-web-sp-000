579d41a414915374e0ec8057c6851a8a9e6ec2c7
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-12 01:10:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  smallest_value = name_hash[key]
+  smallest_value = name_hash[0]
   binding.pry
-  name_hash.reject{|key, value| value > smallest_value}.collect do |key, value|
-    key
+  name_hash.collect do |key, value|
+    if value < smallest_value
+      smallest_value = value
+    
+    end  
+    
   end
 end
